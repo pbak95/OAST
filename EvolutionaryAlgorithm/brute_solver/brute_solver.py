e370f175882b024a66a0d93ca0c779e68c860ad6
Grzegorz Przytula <Grzegorz.Przytula@ipfdigital.com>
2019-04-22 13:42:59 +0200

    Remove mock, unused imports, add 100% progress

--- EvolutionaryAlgorithm/brute_solver/brute_solver.py
+++ EvolutionaryAlgorithm/brute_solver/brute_solver.py
@@ -3,32 +3,10 @@ import itertools
 import math
 import time
 import sys
-from tqdm import tqdm
 
 from model import Network
 
 FALSE_START = False
-
-
-def mock_solution(network) -> Network:
-    links = network.links_list
-    for link in links:
-        link.number_of_fibers = 1
-        link.number_of_signals = 2
-
-    # demands_flow_list = [
-    #     DemandFlow(
-    #         demand_id=1, number_of_demand_paths=1,
-    #         demand_path_flow_list=[DemandPathFlow(path_id=1, path_singal_count=2)]
-    #     ),
-    #     DemandFlow(
-    #         demand_id=2, number_of_demand_paths=1,
-    #         demand_path_flow_list=[DemandPathFlow(path_id=2, path_singal_count=2)]
-    #     )
-    # ]
-    # network.demand_solution = demands_flow_list
-
-    return network
 
 
 def brute_solve(network: Network) -> Network:
