14f207f6ec402f3b8898f74dd7b9d815e438c29e
Grzegorz Przytula <Grzegorz.Przytula@ipfdigital.com>
2019-04-22 11:13:02 +0200

    Remove old files

--- EvolutionaryAlgorithm/parser/__init__.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-from .parser import read_file
-
-logger = logging.getLogger(__name__)
-logger.addHandler(logging.NullHandler())
-
--- EvolutionaryAlgorithm/writer/__init__.py
+++
@@ -1,9 +0,0 @@
-
-import logging
-
-from .writer import write_file
-
-logger = logging.getLogger(__name__)
-logger.addHandler(logging.NullHandler())
-
-
